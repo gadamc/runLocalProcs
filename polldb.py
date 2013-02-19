076cc6c28eb70aa92a4a4d5d6647b67085c3504d
gadamc <adam.cox@kit.edu>
2013-02-19 21:55:58 +0100

    update to kdata 5.01 and using new filter on the couchdb _changes feed

--- polldb.py
+++ polldb.py
@@ -27,8 +27,8 @@ def callback(inputLine):
     print str(datetime.datetime.now())
     print json.dumps(inputLine,indent=1)
     #print 'waiting for ten minutes before starting data transfer to be sure that the entire data file has been copied to the local disk. We do this because we depend upon the ana scripts that copy data from the samba machines to S7'
-    print 'ten minute sleep to ensure data transfer complete'
-    time.sleep(600)
+    print 'five minute sleep to ensure data transfer complete'
+    time.sleep(300)
     print ''
     rp.run(**creds)
 
@@ -36,4 +36,8 @@ s = Server(creds['server'])
 db = s[creds['database']]
 c = Consumer(db)
 print 'starting continuous polling....'
-c.wait(callback, since = db.info()['update_seq'], filter='proc/newproc0', feed='continuous', heartbeat=60000)  
+c.wait(callback, since = db.info()['update_seq'], filter='proc/newProc0orMetaProc0', feed='continuous', heartbeat=60000)  
+
+#if we want to be conservative, we can switch to only running when a new log file shows up. comment the line above and uncomment the line below
+
+#c.wait(callback, since = db.info()['update_seq'], filter='proc/newdaqlogfile', feed='continuous', heartbeat=60000)  
